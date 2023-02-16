17b4c3444699c8132e0b5fef23bdd439d30b9206
vladimirmovsesan <vladimirmovsesan-dev@yandex.ru>
2023-02-16 23:56:34 +0300

    Some code decomposition and fixes after code review

--- main.py
+++ main.py
@@ -7,12 +7,11 @@ import mysql.connector
 from mysql.connector import Error
 
 
-def create_db_connection(host_name, user_name, user_password, db_name):
+def sql_create_db_connection(host_name, user_password, db_name):
     connection = None
     try:
         connection = mysql.connector.connect(
             host=host_name,
-            # user=user_name,
             passwd=user_password,
             database=db_name
         )
@@ -23,7 +22,7 @@ def create_db_connection(host_name, user_name, user_password, db_name):
     return connection
 
 
-def execute_query(connection, query):
+def sql_execute_query(connection, query):
     cursor = connection.cursor()
     try:
         cursor.execute(query)
@@ -33,15 +32,61 @@ def execute_query(connection, query):
         print(f"Error: '{err}'")
 
 
-def read_query(connection, query):
+def sql_read_query(connection, query):
     cursor = connection.cursor()
     result = None
     try:
         cursor.execute(query)
         result = cursor.fetchall()
-        return result
     except Error as err:
         print(f"Error: '{err}'")
+
+    return result
+
+
+def user_role_check(connection, user_id, role):
+    return True if sql_read_query(connection, f"SELECT {role}_id FROM {role}s WHERE {role}_id = {user_id}") else False
+
+
+def user_message_handler(event, vk_session):
+    # getting user_id
+    user_id = event.obj["message"]["from_id"]
+
+    # getting users name
+    response = vk_session.method(method="users.get", values={"user_ids": user_id})
+    user_vk = (response[0]['first_name'], response[0]['last_name'])
+
+    # working with database
+    user_database_handler(event, vk_session, user_id, user_vk)
+
+    # printing data to terminal
+    print(f'Who: {user_vk[0]} {user_vk[1]} |id: {user_id}|')
+    print(f'Text:', event.obj["message"]["text"])
+
+
+def user_database_handler(event, vk_session, user_id, user_vk):
+    # creating database connection
+    db = sql_create_db_connection("localhost", "admin", "vk_service_bot")
+
+    # adding new client to database
+    if not user_role_check(db, user_id, "client"):
+        sql_execute_query(db, f"INSERT INTO clients VALUES ({user_id}, '{user_vk[0]}', '{user_vk[1]}')")
+
+    # TODO: Add errors handler
+    # adding new author to database
+    if (event.obj["message"]["text"].split())[0] == '/add_author':
+        command, author_id = (event.obj["message"]["text"].split())
+
+        # check if user is admin
+        if user_role_check(db, user_id, "admin") and not user_role_check(db, author_id, "author"):
+            # TODO: Very expensive operation :(
+            author_response = vk_session.method(method="users.get", values={"user_ids": author_id})
+            sql_execute_query(db, f"INSERT INTO authors VALUES ({author_id}, "
+                                  f"'{author_response[0]['first_name']}', "
+                                  f"'{author_response[0]['last_name']}')")
+
+    # closing database connection
+    db.close()
 
 
 def main():
@@ -54,43 +99,7 @@ def main():
         # print(event.obj)
 
         if event.type == VkBotEventType.MESSAGE_NEW:
-            # getting user_id
-            user_id = event.obj["message"]["from_id"]
-
-            # getting users name
-            response = vk_session.method(method="users.get", values={"user_ids": user_id})
-            user_vk = (response[0]['first_name'], response[0]['last_name'])
-
-            # creating database connection
-            db = create_db_connection("localhost", "", "admin", "vk_service_bot")
-
-            # adding new client to database
-            clients_id = [client[0] for client in read_query(db, "SELECT client_id FROM clients")]
-            if user_id not in clients_id:
-                execute_query(db, f"INSERT INTO clients VALUES ({user_id}, '{user_vk[0]}', '{user_vk[1]}')")
-
-            # TODO: Add errors handler
-            # adding new author to database
-            if (event.obj["message"]["text"].split())[0] == '/add_author':
-                command, author_id = (event.obj["message"]["text"].split())
-
-                # getting all id's of admins
-                admins_id = [admin[0] for admin in read_query(db, "SELECT admin_id FROM admins")]
-
-                # check if user is admin
-                if user_id in admins_id:
-                    # TODO: Very expensive operation :(
-                    author_response = vk_session.method(method="users.get", values={"user_ids": author_id})
-                    execute_query(db, f"INSERT INTO authors VALUES ({author_id}, "
-                                      f"'{author_response[0]['first_name']}', "
-                                      f"'{author_response[0]['last_name']}')")
-
-            # closing database connection
-            db.close()
-
-            # printing data to terminal
-            print(f'Who: {user_vk[0]} {user_vk[1]} |id: {user_id}|')
-            print(f'Text:', event.obj["message"]["text"])
+            user_message_handler(event, vk_session)
         else:
             print(event.type)
 
