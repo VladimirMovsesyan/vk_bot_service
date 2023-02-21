67b9ffc2c619c1a8478c003b984c18d0721a34fd
vladimirmovsesan <vladimirmovsesan-dev@yandex.ru>
2023-02-22 01:09:00 +0300

    Added support of forwarding photos. Some code refactor. Some TODO's added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 
 # vk_api
 import vk_api
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.utils import get_random_id
 
@@ -12,6 +13,7 @@ import mysql.connector
 from mysql.connector import Error
 
 
+# TODO: Move DataBase class to another file
 class DataBase:
     db: mysql.connector.pooling.PooledMySQLConnection | \
         mysql.connector.connection.MySQLConnection | \
@@ -110,9 +112,15 @@ class VkBot:
         if event.obj["message"]["text"]:
             self.user_command_handler(event, db)
 
-        # sending message to other member of dialog if it exists
+        # TODO: add support of forwarding another data from message (such as gifs, docs, music, etc.)
+        # forwarding message to other member of dialog if it exists
         if db.is_connected(event.obj["message"]["from_id"]):
-            self.echo(event.obj["message"]["text"], db.get_companion(event.obj["message"]["from_id"]))
+            keyboard = VkKeyboard(one_time=True)
+            keyboard.add_button('/disconnect', VkKeyboardColor.NEGATIVE)
+            self.forward_message(message=event.obj["message"]["text"],
+                                 user_id=db.get_companion(event.obj["message"]["from_id"]),
+                                 keyboard=keyboard.get_keyboard(),
+                                 attachments=event.obj["message"]["attachments"])
 
         # closing database connection
         db.close()
@@ -137,8 +145,8 @@ class VkBot:
             commands_dict[command](event, db)
 
     def invalid_command(self, event: vk_api.bot_longpoll.VkBotMessageEvent, valid_command: str):
-        self.echo(message=f'Неверно использована команда, верное использование:\n' + valid_command,
-                  user_id=event.obj["message"]["from_id"])
+        self.send_message(message=f'Неверно использована команда, верное использование:\n' + valid_command,
+                          user_id=event.obj["message"]["from_id"])
 
     # commands
     def add_author(self, event: vk_api.bot_longpoll.VkBotMessageEvent, db: DataBase) -> None:
@@ -153,22 +161,24 @@ class VkBot:
         if db.user_role_check(event.obj["message"]["from_id"], "admin") and \
                 not db.user_role_check(author_id, "author"):
             db.sql_execute_query(f'INSERT INTO author VALUES ({author_id})')
-            self.echo(message=f'Автор с {author_personal_id} был добавлен!', user_id=event.obj["message"]["from_id"])
+            self.send_message(message=f'Автор с {author_personal_id} был добавлен!',
+                              user_id=event.obj["message"]["from_id"])
         else:
             if db.user_role_check(event.obj["message"]["from_id"], "admin"):
-                self.echo(message=f'Автор с {author_personal_id} уже существовал!',
-                          user_id=event.obj["message"]["from_id"])
+                self.send_message(message=f'Автор с {author_personal_id} уже существовал!',
+                                  user_id=event.obj["message"]["from_id"])
             else:
-                self.echo(message=f'У вас недостаточно прав для этой команды!',
-                          user_id=event.obj["message"]["from_id"])
+                self.send_message(message=f'У вас недостаточно прав для этой команды!',
+                                  user_id=event.obj["message"]["from_id"])
 
     def get_authors(self, event: vk_api.bot_longpoll.VkBotMessageEvent, db: DataBase) -> None:
         # check if user is admin
         if db.user_role_check(event.obj["message"]["from_id"], "admin"):
-            self.echo(message='Список авторов:', user_id=event.obj["message"]["from_id"])
-            self.echo(message=str(db.sql_read_query('SELECT * FROM author')), user_id=event.obj["message"]["from_id"])
+            self.send_message(message='Список авторов:\n' + str(db.sql_read_query('SELECT * FROM author')),
+                              user_id=event.obj["message"]["from_id"])
         else:
-            self.echo(message='У вас недостаточно прав для этой команды!', user_id=event.obj["message"]["from_id"])
+            self.send_message(message='У вас недостаточно прав для этой команды!',
+                              user_id=event.obj["message"]["from_id"])
 
     def delete_author(self, event: vk_api.bot_longpoll.VkBotMessageEvent, db: DataBase) -> None:
         if len(event.obj["message"]["text"].split()) < 2:
@@ -181,13 +191,15 @@ class VkBot:
         # check if user is admin
         if db.user_role_check(event.obj["message"]["from_id"], "admin") and db.user_role_check(author_id, "author"):
             db.sql_execute_query(f'DELETE FROM author WHERE author_id = {author_id}')
-            self.echo(message=f'Автор {author_personal_id} был удален!', user_id=event.obj["message"]["from_id"])
+            self.send_message(message=f'Автор {author_personal_id} был удален!',
+                              user_id=event.obj["message"]["from_id"])
         else:
             if db.user_role_check(event.obj["message"]["from_id"], "admin"):
-                self.echo(message=f'Автора {author_personal_id} не существует!',
-                          user_id=event.obj["message"]["from_id"])
+                self.send_message(message=f'Автора {author_personal_id} не существует!',
+                                  user_id=event.obj["message"]["from_id"])
             else:
-                self.echo(message=f'У вас недостаточно прав!', user_id=event.obj["message"]["from_id"])
+                self.send_message(message=f'У вас недостаточно прав!',
+                                  user_id=event.obj["message"]["from_id"])
 
     def add_admin(self, event: vk_api.bot_longpoll.VkBotMessageEvent, db: DataBase) -> None:
         if len(event.obj["message"]["text"].split()) < 2:
@@ -201,22 +213,23 @@ class VkBot:
         if db.user_role_check(event.obj["message"]["from_id"], "admin") and \
                 not db.user_role_check(admin_id, "admin"):
             db.sql_execute_query(f'INSERT INTO admin VALUES ({admin_id})')
-            self.echo(message=f'Администратор {admin_personal_id} был добавлен!',
-                      user_id=event.obj["message"]["from_id"])
+            self.send_message(message=f'Администратор {admin_personal_id} был добавлен!',
+                              user_id=event.obj["message"]["from_id"])
         else:
             if db.user_role_check(event.obj["message"]["from_id"], "admin"):
-                self.echo(message=f'Администратор {admin_personal_id} уже существует!',
-                          user_id=event.obj["message"]["from_id"])
+                self.send_message(message=f'Администратор {admin_personal_id} уже существует!',
+                                  user_id=event.obj["message"]["from_id"])
             else:
-                self.echo(message=f'У вас недостаточно прав!', user_id=event.obj["message"]["from_id"])
+                self.send_message(message=f'У вас недостаточно прав!',
+                                  user_id=event.obj["message"]["from_id"])
 
     def get_admins(self, event: vk_api.bot_longpoll.VkBotMessageEvent, db: DataBase) -> None:
         # check if user is admin
         if db.user_role_check(event.obj["message"]["from_id"], "admin"):
-            self.echo(message=f'Список администраторов:', user_id=event.obj["message"]["from_id"])
-            self.echo(message=str(db.sql_read_query('SELECT * FROM admin')), user_id=event.obj["message"]["from_id"])
+            self.send_message(message='Список администраторов:\n' + str(db.sql_read_query('SELECT * FROM admin')),
+                              user_id=event.obj["message"]["from_id"])
         else:
-            self.echo(message=f'У вас недостаточно прав!', user_id=event.obj["message"]["from_id"])
+            self.send_message(message=f'У вас недостаточно прав!', user_id=event.obj["message"]["from_id"])
 
     def delete_admin(self, event: vk_api.bot_longpoll.VkBotMessageEvent, db: DataBase) -> None:
         if len(event.obj["message"]["text"].split()) < 2:
@@ -229,14 +242,15 @@ class VkBot:
         # check if user is admin
         if db.user_role_check(event.obj["message"]["from_id"], "admin") and db.user_role_check(admin_id, "admin"):
             db.sql_execute_query(f'DELETE FROM admin WHERE admin_id = {admin_id}')
-            self.echo(message=f'Администратор {admin_personal_id} был удален!',
-                      user_id=event.obj["message"]["from_id"])
+            self.send_message(message=f'Администратор {admin_personal_id} был удален!',
+                              user_id=event.obj["message"]["from_id"])
         else:
             if db.user_role_check(event.obj["message"]["from_id"], "admin"):
-                self.echo(message=f'Администратора {admin_personal_id} не существует!',
-                          user_id=event.obj["message"]["from_id"])
+                self.send_message(message=f'Администратора {admin_personal_id} не существует!',
+                                  user_id=event.obj["message"]["from_id"])
             else:
-                self.echo(message=f'У вас недостаточно прав!', user_id=event.obj["message"]["from_id"])
+                self.send_message(message=f'У вас недостаточно прав!',
+                                  user_id=event.obj["message"]["from_id"])
 
     def create_connection(self, event: vk_api.bot_longpoll.VkBotMessageEvent, db: DataBase) -> None:
         if len(event.obj["message"]["text"].split()) < 3:
@@ -250,21 +264,37 @@ class VkBot:
         # TODO: Add handler of unique connection
         # check if user is admin
         if db.user_role_check(event.obj["message"]["from_id"], "admin"):
+            keyboard = VkKeyboard(one_time=True)
+            keyboard.add_button('/disconnect', VkKeyboardColor.NEGATIVE)
+
             db.sql_execute_query(f'INSERT INTO connection(client_id, author_id) VALUES({client_id}, {author_id})')
-            self.echo(message=f'Связь между клиентом: {client_personal_id} и '
-                              f'автором: {author_personal_id} установлена!',
-                      user_id=event.obj["message"]["from_id"])
+
+            self.send_message(message=f'Связь между клиентом: {client_personal_id} и '
+                                      f'автором: {author_personal_id} установлена!',
+                              user_id=event.obj["message"]["from_id"])
+
+            self.forward_message(message='Связь с автором установлена!',
+                                 user_id=client_id,
+                                 keyboard=keyboard.get_keyboard(),
+                                 attachments=event.obj["message"]["attachments"])
+
+            self.forward_message(message='Связь с клиентом установлена!',
+                                 user_id=author_id,
+                                 keyboard=keyboard.get_keyboard(),
+                                 attachments=event.obj["message"]["attachments"])
         else:
-            self.echo(message=f'У вас недостаточно прав!', user_id=event.obj["message"]["from_id"])
+            self.send_message(message=f'У вас недостаточно прав!',
+                              user_id=event.obj["message"]["from_id"])
 
     def get_connections(self, event: vk_api.bot_longpoll.VkBotMessageEvent, db: DataBase) -> None:
         # check if user is admin
         if db.user_role_check(event.obj["message"]["from_id"], "admin"):
-            self.echo(message='Список установленных соединений:', user_id=event.obj["message"]["from_id"])
-            self.echo(message=str(db.sql_read_query('SELECT * FROM connection')),
-                      user_id=event.obj["message"]["from_id"])
+            self.send_message(message='Список установленных соединений:\n' +
+                                      str(db.sql_read_query('SELECT * FROM connection')),
+                              user_id=event.obj["message"]["from_id"])
         else:
-            self.echo(message=f'У вас недостаточно прав!', user_id=event.obj["message"]["from_id"])
+            self.send_message(message=f'У вас недостаточно прав!',
+                              user_id=event.obj["message"]["from_id"])
 
     def delete_connection(self, event: vk_api.bot_longpoll.VkBotMessageEvent, db: DataBase) -> None:
         if len(event.obj["message"]["text"].split()) < 3:
@@ -275,25 +305,42 @@ class VkBot:
         client_id = self.vk_session.method(method="users.get", values={"user_ids": client_personal_id})[0]["id"]
         author_id = self.vk_session.method(method="users.get", values={"user_ids": author_personal_id})[0]["id"]
 
+        # TODO: Check if connection exists before deletion
         # check if user is admin
         if db.user_role_check(event.obj["message"]["from_id"], "admin"):
             db.sql_execute_query(f'DELETE FROM connection WHERE client_id = {client_id} AND author_id = {author_id}')
-            self.echo(message=f'Связь между клиентом: {client_personal_id} и автором: {author_personal_id} прервана!',
-                      user_id=event.obj["message"]["from_id"])
+            self.send_message(message=f'Связь между клиентом: {client_personal_id} и '
+                                      f'автором: {author_personal_id} прервана!',
+                              user_id=event.obj["message"]["from_id"])
         else:
-            self.echo(message=f'У вас недостаточно прав!', user_id=event.obj["message"]["from_id"])
+            self.send_message(message=f'У вас недостаточно прав!',
+                              user_id=event.obj["message"]["from_id"])
 
     def disconnect(self, event: vk_api.bot_longpoll.VkBotMessageEvent, db: DataBase) -> None:
         db.sql_execute_query(f'DELETE FROM connection WHERE client_id = {event.obj["message"]["from_id"]} OR '
                              f'author_id = {event.obj["message"]["from_id"]}')
-        self.echo('Вы отключились от чата!', user_id=event.obj["message"]["from_id"])
+        self.send_message('Вы отключились от чата!',
+                          user_id=event.obj["message"]["from_id"])
 
-    # TODO: Rename
-    def echo(self, message: str, user_id: int) -> None:
+    def send_message(self, message: str, user_id: int) -> None:
         self.vk_session.get_api().messages.send(
             user_id=user_id,
             random_id=get_random_id(),
             message=message
+        )
+
+    def forward_message(self, message: str, user_id: int, keyboard: str, attachments: list) -> None:
+        attachment = [
+            f'{attach["type"]}{attach[attach["type"]]["owner_id"]}_{attach[attach["type"]]["id"]}' +
+            f'_{attach[attach["type"]]["access_key"]}'
+            for attach in attachments
+        ]
+        self.vk_session.get_api().messages.send(
+            user_id=user_id,
+            random_id=get_random_id(),
+            keyboard=keyboard,
+            message=message,
+            attachment=attachment
         )
 
 
